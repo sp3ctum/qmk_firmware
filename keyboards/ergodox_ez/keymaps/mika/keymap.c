6ee8eb25ec6063429eb03febaa0120df97ea2887
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-22 10:43:28 +0300

    Add volume minus, plus and mute keys to number layer

--- keyboards/ergodox_ez/keymaps/mika/keymap.c
+++ keyboards/ergodox_ez/keymaps/mika/keymap.c
@@ -86,9 +86,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |      |      |      |      |      |                                       |   0  |    . |      |   =  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
+ *                                        |      |      |       | vol- | vol- |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |      |       | mute |      |      |
  *                                 |      |      |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
@@ -110,8 +110,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_DOWN, KC_4,   KC_5,    KC_6,    NO_PLUS, KC_TRNS,
        KC_TRNS, NO_AMPR, KC_1,   KC_2,    KC_3,    NO_BSLS, KC_TRNS,
                          KC_0,   KC_DOT,  KC_TRNS, NO_EQL,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
+       KC_VOLD, KC_VOLU,
+       KC_MUTE,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
