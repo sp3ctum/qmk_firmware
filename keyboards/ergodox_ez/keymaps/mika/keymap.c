ba807a354d718a9dc29fa78c84abd7d19d680e40
Mika Vilpas <mika.vilpas@gmail.com>
2019-10-17 15:15:58 +0300

    Support for proper mouse buttons

--- keyboards/ergodox_ez/keymaps/mika/keymap.c
+++ keyboards/ergodox_ez/keymaps/mika/keymap.c
@@ -253,7 +253,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_WH_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
          KC_TRNS, KC_TRNS,
          KC_TRNS,
-         KC_TRNS, KC_APP,  KC_KP_5
+         KC_BTN3, KC_BTN2,  KC_BTN1
          )
 };
 
